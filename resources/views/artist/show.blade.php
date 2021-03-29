da5fae333a7664b7ceb889e84034b73c997daf5f
Florent Marcel <flo.marcel@hotmail.com>
2021-03-30 00:24:31 +0200

    added a message when the id of an artist is out of bound

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Fiche d\'un artiste')
 
 @section('content')
-    <h1>{{ $artist->firstname }} {{ $artist->lastname }}</h1>
-    <nav><a href="{{ route('artist_index') }}"->Retour à l'index</a></nav>
+    @if ($artist != null)
+        <h1>{{ $artist->firstname }} {{ $artist->lastname }}</h1>
+        <nav><a href="{{ route('artist_index') }}"->Retour à l'index</a></nav>
+    @else
+    <h1>Artiste pas trouvé</h1>
+    @endif
 @endsection
