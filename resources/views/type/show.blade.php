7cd25948b88262de366dbbf00e46d3086ab61206
Florent-Marcel <flo.marcel@hotmail.com>
2021-04-01 23:36:18 +0200

    added a message when the id of type is out of bounds

--- resources/views/type/show.blade.php
+++ resources/views/type/show.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Fiche d\'un type')
 
 @section('content')
-    <h1> {{ $type->type}} </h1>
-    <nav><a href="{{route('type_index') }}">Retour à l'index</a></nav>
+    @if (!empty ($type))
+        <h1> {{ $type->type}} </h1>
+        <nav><a href="{{route('type_index') }}">Retour à l'index</a></nav>
+    @else
+        <h1>Il n'y a pas d'enregistrement </h1>
+    @endif
 @endsection
