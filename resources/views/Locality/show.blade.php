02110ef22a771c8ed39b394ce20e17a0fea1e8c7
Patrice Verstichel <patrice.verstichel@gmail.com>
2021-04-22 07:35:58 +0200

    Controller Locality +template+route

--- resources/views/Locality/show.blade.php
+++ resources/views/Locality/show.blade.php
@@ -6,6 +6,11 @@
 @section('content')
     @if (! empty ($locality))
       <h1>{{ $locality->locality }} {{ $locality->postal_code }}</h1>
+      <ul>
+        @foreach($locality->locations as $location)
+            <li>{{ $location->designation }}</li>
+        @endforeach
+        </ul>
       <nav><a href="{{ route('locality_index') }}">Retour à l'index</a></nav>
     @else
         <h1>Il n'y a pas d'enregistrement </h1>
--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/role', [RoleController::class, 'index'])->name('role_index');
 Route::get('/role/{id}', [RoleController::class, 'show'])
         ->where('id', '[0-9]+')->name('role_show');
 
-        Route::get('/locality', [LocalityController::class, 'index'])->name('locality_index');
+Route::get('/locality', [LocalityController::class, 'index'])->name('locality_index');
 Route::get('/locality/{id}', [LocalityController::class, 'show'])
         ->where('id', '[0-9]+')->name('locality_show');
 
