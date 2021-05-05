26fa8b5f43b9906ba467b7d0b013d4211b002954
Patrice Verstichel <patrice.verstichel@gmail.com>
2021-05-05 23:00:38 +0200

    Relation ManyToMany between User,Role & Representation

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class UserRole extends Model
 {
     use HasFactory;
+    protected $fillable = [
+       'user_id','role_id'
+    ];
+    protected $table = 'user_id';
+    public $timestamps = false;
+
+    /**
+     * Get the role of the user
+     */
+    public function role()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    /**
+     * Get user for this role
+     */
+    public function user()
+    {
+        return $this->belongToMany(Role::class);
+    }
+
 }
---
+++ database/seeders/RepresentationUserSeeder.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Representation;
+use App\Models\Location;
+use App\Models\Show;
+
+class RepresentationUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Empty the table first
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('user_role')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
+        //Define data
+        $representationUsers =[
+            [
+                'user_login'=>'Florent',
+                'location_slug'=>'espace-delvaux-la-venerie',
+                'show_slug'=>'ayiyi',
+                'representation_when'=>'2012-10-12 13:30:00',
+                'places'=>300
+            ],
+            [
+                'user_login'=>'Patrice',
+                'location_slug'=>'dexia-art-center',
+                'show_slug'=>'ayiyi',
+                'representation_when'=>'2012-10-12 20:30:00',
+                'places'=>250
+            ]
+            ];
+
+        //Prepare the data
+        foreach ($representationUsers as &$data){
+            //search the user for a given username
+            $user = User::where([
+                ['login','=',$data['user_login']]
+            ])->first();
+
+            $location = Location::where([
+                ['slug','=',$data['location_slug']]
+            ])->first();
+
+            $show = Show::where([
+                ['slug','=',$data['show_slug']]
+            ])->first();
+
+            $representation = Representation::where([
+                ['location_id','=',$location->id],
+                ['show_id','=',$show->id],
+                ['when','=',$data['representation_when']]
+            ])->first();
+
+            unset($data['user_login']);
+            unset($data['location_slug']);
+            unset($data['show_slug']);
+            unset($data['representation_when']);
+
+            $data['user_id'] = $user->id;
+
+            $data['representation_id'] = $representation->id;
+
+        }
+        unset($data);
+
+        DB::table('representation_user')->insert($representationUsers);
+
+    }
+}
