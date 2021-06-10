5fb401b2c4a48f3154f4ba8d354df9b6abc6b948
Florent-Marcel <flo.marcel@hotmail.com>
2021-06-11 01:49:47 +0200

    datatables for reservations, limit the diplay of reservation on the user and min 1 for reservation

--- resources/views/reservation/index.blade.php
+++ resources/views/reservation/index.blade.php
@@ -1,8 +1,15 @@
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="{{ URL::asset('js/dataTables.js')}}"></script>
+</head>
+
 <x-app-layout>
     <x-slot name="header">
         <h1>Liste des {{ $resource }}</h1>
     </x-slot>
-    <table>
+    <table id="table" class="display">
         <thead>
             <tr>
                 <th>Réservations</th>
@@ -18,23 +25,34 @@
         <tbody>
         @foreach($reservations as $reservation)
             <tr>
-                <td><a class="hover:text-blue-600" href="{{ route('reservation_show', $reservation->id) }}">{{ $reservation->id}}
-                <td>{{ $reservation->representation->when }}</a></td>
-                <td>{{ $reservation->representation->show->title}}</td>
+                <td class="text-left"><a class="hover:text-blue-600" href="{{ route('reservation_show', $reservation->id) }}">{{ $reservation->id}}
+                <td class="text-left">{{ $reservation->representation->when }}</a></td>
+                <td class="text-left">{{ $reservation->representation->show->title}}</td>
                 @if ($reservation->representation->location != NULL)
-                    <td>{{ $reservation->representation->location->designation}}</td>
-                    <td>{{ $reservation->representation->location->locality->postal_code}}</td>
-                    <td>{{ $reservation->representation->location->locality->locality}}</td>
+                    <td class="text-left">{{ $reservation->representation->location->designation}}</td>
+                    <td class="text-left">{{ $reservation->representation->location->locality->postal_code}}</td>
+                    <td class="text-left">{{ $reservation->representation->location->locality->locality}}</td>
                 @else
-                    <td>Aucune désignation</td>
-                    <td></td>
-                    <td></td>
+                    <td class="text-left">Aucune désignation</td>
+                    <td class="text-left"></td>
+                    <td class="text-left"></td>
                 @endif
-                <td><a class="hover:text-blue-600" href="{{ route('reservation_show', $reservation->user_id) }}">{{ $reservation->user->login }} </a></td>
-                <td>{{ $reservation->places }} </td>
-
+                <td class="text-left"><a class="hover:text-blue-600" href="{{ route('reservation_show', $reservation->user_id) }}">{{ $reservation->user->login }} </a></td>
+                <td class="text-left">{{ $reservation->places }} </td>
             </tr>
         @endforeach
         </tbody>
+        <tfoot>
+            <tr>
+                <th>Réservations</th>
+                <th>Dates</th>
+                <th>Shows</th>
+                <th>Locations</th>
+                <th>CP</th>
+                <th>Localité</th>
+                <th>Utilisateur</th>
+                <th>Places réservées</th>
+            </tr>
+        </tfoot>
     </table>
 </x-app-layout>
